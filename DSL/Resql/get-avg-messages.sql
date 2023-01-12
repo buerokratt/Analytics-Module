9ed89c7b25d027a457cc42df9813f71a8262a4b8
gertdrui <gert.drui@gmail.com>
2023-01-12 13:31:22 +0200

    assure all queries only use chats with messages from end-user

--- DSL/Resql/get-avg-messages.sql
+++ DSL/Resql/get-avg-messages.sql
@@ -1,14 +1,21 @@
-WITH chats AS
-(
-    SELECT  DISTINCT base_id
+WITH chats AS (
+    SELECT DISTINCT base_id
     FROM chat
-    WHERE created BETWEEN :start :: timestamptz AND :end :: timestamptz 
+    WHERE created BETWEEN :start::timestamptz AND :end::timestamptz
+        AND EXISTS (
+            SELECT 1
+            FROM message
+            WHERE message.chat_base_id = chat.base_id
+                AND message.author_role = 'end-user'
+        )
 )
-SELECT  AVG(num_messages)
-FROM
-(
-    SELECT  COUNT(DISTINCT base_id) AS num_messages
-    FROM "message"
-    WHERE chat_base_id IN ( SELECT base_id FROM chats )
-    GROUP BY  chat_base_id
-) AS msg_counts;
+SELECT AVG(num_messages)
+FROM (
+        SELECT COUNT(DISTINCT base_id) AS num_messages
+        FROM "message"
+        WHERE chat_base_id IN (
+                SELECT base_id
+                FROM chats
+            )
+        GROUP BY chat_base_id
+    ) AS msg_counts;
--- DSL/Resql/get-chats-total.sql
+++ DSL/Resql/get-chats-total.sql
@@ -1,3 +1,9 @@
 SELECT COUNT(distinct base_id)
 FROM chat
-WHERE created BETWEEN :start :: timestamptz AND :end :: timestamptz;
+WHERE created BETWEEN :start::timestamptz AND :end::timestamptz
+    AND exists (
+        SELECT 1
+        FROM message
+        WHERE message.chat_base_id = chat.base_id
+            AND message.author_role = 'end-user'
+    )

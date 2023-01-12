7e2f0ac82e33efce47590cb08c4c48d3002a9620
gertdrui <gert.drui@gmail.com>
2023-01-12 13:30:47 +0200

    add average time queries for with and without CSA

---
+++ DSL/Resql/get-avg-time-no-csa.sql
@@ -0,0 +1,25 @@
+WITH chats AS (
+    SELECT DISTINCT base_id
+    FROM chat
+    WHERE created BETWEEN :start::timestamptz AND :end::timestamptz
+        AND NOT EXISTS (
+            SELECT 1
+            FROM message
+            WHERE message.chat_base_id = chat.base_id
+                AND message.author_role = 'backoffice-user'
+        )
+        AND EXISTS (
+            SELECT 1
+            FROM message
+            WHERE message.chat_base_id = chat.base_id
+                AND message.author_role = 'end-user'
+        )
+),
+chat_lengths AS (
+    SELECT age(MAX(created), MIN(created)) AS chat_length
+    FROM message
+        JOIN chats ON message.chat_base_id = chats.base_id
+    GROUP BY message.chat_base_id
+)
+SELECT AVG(chat_length)
+FROM chat_lengths;

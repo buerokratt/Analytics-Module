35b31fec56307376e30b13d41b78e626be0330ba
gertdrui <gert.drui@gmail.com>
2023-01-12 13:56:49 +0200

    add query for count of chats with no csa

---
+++ DSL/Resql/get-chats-count-no-csa.sql
@@ -0,0 +1,15 @@
+SELECT COUNT(distinct base_id)
+FROM chat
+WHERE created BETWEEN :start::timestamptz AND :end::timestamptz
+    AND exists (
+        SELECT 1
+        FROM message
+        WHERE message.chat_base_id = chat.base_id
+            AND message.author_role = 'end-user'
+    )
+    AND not exists (
+        SELECT 1
+        FROM message
+        WHERE message.chat_base_id = chat.base_id
+            AND message.author_role = 'backoffice-user'
+    )

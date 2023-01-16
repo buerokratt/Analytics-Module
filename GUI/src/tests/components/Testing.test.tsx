f73597bdfdab58a416bc6cc7146b6f4bc9205866
baha-a <bhlshrf@gmail.com>
2023-01-16 13:26:23 +0300

    Remove test packages for simplicity

--- GUI/src/tests/components/Testing.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Testing from '../../components/Testing'
-
-test('renders current date', () => {
-    render(<Testing />)
-    const text = screen.getByText(/testing data:/i)
-    expect(text).toBeInTheDocument()
-})
--- GUI/src/tests/pages/HomePage.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import HomePage from '../../pages/HomePage'
-
-test('renders hello world message', () => {
-    render(<HomePage />)
-    const name = screen.getByText(/Analytics Module/i)
-    expect(name).toBeInTheDocument()
-})

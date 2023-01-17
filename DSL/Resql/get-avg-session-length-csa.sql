d87702e0d517cfd6d430b4365ad0cd64feb63d3a
gertdrui <gert.drui@gmail.com>
2023-01-17 09:50:09 +0200

    add session length metrics

---
+++ DSL/Resql/get-avg-session-length-csa.sql
@@ -0,0 +1,29 @@
+WITH chats AS (
+    SELECT base_id,
+        ended
+    FROM chat
+    WHERE created BETWEEN :start::timestamptz AND :end::timestamptz
+        AND EXISTS (
+            SELECT 1
+            FROM message
+            WHERE message.chat_base_id = chat.base_id
+                AND message.author_role = 'backoffice-user'
+        )
+        AND EXISTS (
+            SELECT 1
+            FROM message
+            WHERE message.chat_base_id = chat.base_id
+                AND message.author_role = 'end-user'
+        )
+        AND ended IS NOT NULL
+    GROUP BY base_id,
+        ended
+),
+chat_lengths AS (
+    SELECT age(MAX(chats.ended), MIN(created)) AS chat_length
+    FROM message
+        JOIN chats ON message.chat_base_id = chats.base_id
+    GROUP BY message.chat_base_id
+)
+SELECT AVG(chat_length)
+FROM chat_lengths;

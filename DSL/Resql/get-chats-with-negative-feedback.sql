a4c14e96cf53e6fff8d1055426d9e010a851a317
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-01-17 13:08:18 +0200

    - Added Date Period To Negative Chats - Added Negative Chats Ruuter DSL

--- DSL/Resql/get-chats-with-negative-feedback.sql
+++ DSL/Resql/get-chats-with-negative-feedback.sql
@@ -1,13 +1,6 @@
-WITH chats AS (
-   SELECT id, status, feedback_rating, created
-   FROM chat
-   WHERE feedback_rating IS NOT NULL
-   AND status = 'ENDED'
-   AND feedback_rating::int <= 5
-)
-SELECT * FROM chats
-WHERE 
-CASE 
-  WHEN :value = 'today' THEN created::date = current_date
-  WHEN :value = 'yesterday' THEN created::date = current_date - 1
-END;
+SELECT id, feedback_rating, created
+FROM chat
+WHERE feedback_rating IS NOT NULL
+AND status = 'ENDED'
+AND feedback_rating::int <= 5
+AND created::date BETWEEN :start::date AND :end::date;

7db926a7c2fac42e22d020f858c769c16db2233e
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-01-17 02:40:06 +0200

    - Added Period Metrics for today & yesterday

--- DSL/Resql/get-chats-with-negative-feedback.sql
+++ DSL/Resql/get-chats-with-negative-feedback.sql
@@ -1,5 +1,13 @@
-SELECT COUNT(DISTINCT id) 
-FROM chat
-WHERE feedback_rating IS NOT NULL
-AND status = 'ENDED'
-AND feedback_rating::int <= 5;
+WITH chats AS (
+   SELECT id, status, feedback_rating, created
+   FROM chat
+   WHERE feedback_rating IS NOT NULL
+   AND status = 'ENDED'
+   AND feedback_rating::int <= 5
+)
+SELECT * FROM chats
+WHERE 
+CASE 
+  WHEN :value = 'today' THEN created::date = current_date
+  WHEN :value = 'yesterday' THEN created::date = current_date - 1
+END;

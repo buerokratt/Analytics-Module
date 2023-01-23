57ec727db11dcb6630a70e9de7ce478bc78450c5
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-01-23 13:17:50 +0200

    - Modified CSA Chat Count SQL

--- DSL/Resql/get-chat-count-csa.sql
+++ DSL/Resql/get-chat-count-csa.sql
@@ -6,3 +6,9 @@ WHERE created BETWEEN :start::timestamptz AND :end::timestamptz
      FROM message
      WHERE message.chat_base_id = chat.base_id
        AND message.author_role = 'backoffice-user')
+  AND EXISTS
+    (SELECT 1
+     FROM message
+     WHERE message.chat_base_id = chat.base_id
+       AND message.author_role = 'end-user' )
+              

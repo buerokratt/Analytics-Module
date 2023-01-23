24457835b5b3521162025e6e66f72630590bc426
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-01-23 22:26:50 +0200

    - Modified Chats With Negative Feedback

--- DSL/Resql/get-chats-with-negative-feedback.sql
+++ DSL/Resql/get-chats-with-negative-feedback.sql
@@ -1,6 +1,8 @@
-SELECT id, feedback_rating, created
+SELECT date_trunc(:metric, created) AS date_time,
+       COUNT(DISTINCT base_id)
 FROM chat
 WHERE feedback_rating IS NOT NULL
-AND status = 'ENDED'
-AND feedback_rating::int <= 5
-AND created::date BETWEEN :start::date AND :end::date;
+  AND status = 'ENDED'
+  AND feedback_rating::int <= 5
+  AND created::date BETWEEN :start::date AND :end::date
+GROUP BY 1

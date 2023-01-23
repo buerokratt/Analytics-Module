678b65ba71e645b86e8fee9c34dac3b7291c8541
baha-a <bhlshrf@gmail.com>
2023-01-23 10:29:10 +0300

    Change DSL type to POST

--- GUI/src/resources/api-constants.ts
+++ GUI/src/resources/api-constants.ts
@@ -3,3 +3,24 @@ const baseUrl = process.env.REACT_APP_API_URL
 export const getTesting = (): string => {
     return baseUrl + '/testing'
 }
+
+export const getAvgResponseTime = (): string => {
+    return baseUrl + '/avg-response-time'
+}
+
+export const getAvgSessionLengthClientLeft = (): string => {
+    return baseUrl + '/avg-session-length-client-left'
+}
+
+export const getAvgSessionLengthCsa = (): string => {
+    return baseUrl + '/avg-session-length-csa'
+}
+
+export const getAvgSessionLengthNoCsa = (): string => {
+    return baseUrl + '/avg-session-length-no-csa'
+}
+
+export const getPercentCurrectlyUnderstood = (): string => {
+    return baseUrl + '/pct-currectly-understood'
+}
+

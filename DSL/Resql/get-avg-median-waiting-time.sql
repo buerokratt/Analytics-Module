6d71355fdec625aa31c60157352c6ceaee4d729f
baha-a <bhlshrf@gmail.com>
2023-01-24 01:21:16 +0300

    Add metric DSL and missing Resql files

---
+++ DSL/Resql/get-avg-median-waiting-time.sql
@@ -0,0 +1,43 @@
+WITH botname AS (
+    SELECT "value"
+    FROM "configuration"
+    WHERE "key" = 'bot_institution_id'
+    LIMIT 1
+), customer_support_changes AS (
+    SELECT base_id,
+        customer_support_id,
+        updated,
+        lag(customer_support_id) over (
+            PARTITION by base_id
+            ORDER BY updated
+        ) AS prev_support_id,
+        lag(updated) over (
+            PARTITION by base_id
+            ORDER BY updated
+        ) AS prev_updated
+    FROM chat
+    WHERE created BETWEEN :start::timestamptz AND :end::timestamptz
+)
+
+WITH waitingtimes AS (
+    SELECT COALESCE(
+        extract(
+            epoch
+            FROM (updated - prev_updated)
+        ),
+        0
+    ) AS waiting_time_seconds
+    FROM customer_support_changes
+    WHERE prev_support_id = '' 
+    AND customer_support_id NOT IN (
+        (
+            SELECT "value"
+            FROM botname
+        ),
+        ''
+    )
+)
+
+SELECT PERCENTILE_DISC(0.5) AS avg_median_waiting_time_seconds
+WITHIN GROUP(ORDER BY waiting_time_seconds) 
+FROM waitingtimes;

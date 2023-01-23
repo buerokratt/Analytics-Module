2a08ba395a98f16f489c833e766fff8a19f81573
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-01-23 21:46:45 +0200

    - Modified CSA Chat Count

--- DSL/Resql/csa-get-chat-count-csa.sql
+++ DSL/Resql/csa-get-chat-count-csa.sql
@@ -1,7 +1,7 @@
-SELECT COUNT(DISTINCT base_id)
+SELECT date_trunc(:metric, created) AS date_time,
+       COUNT(DISTINCT base_id)
 FROM chat
-WHERE created BETWEEN :start::timestamptz AND :end::timestamptz
-  AND EXISTS
+WHERE EXISTS
     (SELECT 1
      FROM message
      WHERE message.chat_base_id = chat.base_id
@@ -10,5 +10,7 @@ WHERE created BETWEEN :start::timestamptz AND :end::timestamptz
     (SELECT 1
      FROM message
      WHERE message.chat_base_id = chat.base_id
-       AND message.author_role = 'end-user' )
-       
+       AND message.author_role = 'end-user')
+  AND created::date BETWEEN :start::date AND :end::date
+GROUP BY 1
+

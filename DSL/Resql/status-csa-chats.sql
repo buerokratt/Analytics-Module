e5775ce0463529422ee0c7423023a708f0dce70a
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-01-25 16:11:04 +0200

    - Modified Closed Chats Statues

---
+++ DSL/Resql/status-csa-chats.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT chat.base_id,
+       first_value(chat.created) over (
+       PARTITION by chat.base_id
+       ORDER BY chat.updated
+       ) AS date_time,
+       message.event
+  FROM chat join message on chat.base_id = message.chat_base_id
+  AND status = 'ENDED'
+  AND message.event in (:events)
+  AND message.author_role = 'backoffice-user'
+  AND chat.created::date BETWEEN :start::date AND :end::date
+  ORDER BY date_time
+  

16e6ca3f133ba513df33874a9a4672be0902a1a8
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-02-02 11:06:32 +0200

    - Modified All - CSA Chats SQL to fit the graph requirement

--- DSL/Resql/status-csa-chats.sql
+++ DSL/Resql/status-csa-chats.sql
@@ -1,12 +1,9 @@
-SELECT DISTINCT chat.base_id,
-       first_value(chat.created) OVER (
-       PARTITION BY chat.base_id
-       ORDER BY chat.updated
-       ) AS date_time,
-       message.event
-  FROM chat JOIN message ON chat.base_id = message.chat_base_id
-  AND status = 'ENDED'
-  AND message.event IN (:events)
-  AND message.author_role = 'backoffice-user'
-  AND chat.created::date BETWEEN :start::date AND :end::date
-  ORDER BY date_time
+SELECT message.event AS event,
+       COUNT(DISTINCT chat.base_id)
+FROM chat JOIN message ON chat.base_id = message.chat_base_id
+AND status = 'ENDED'
+AND message.event IN (:events)
+AND message.author_role = 'backoffice-user'
+AND chat.created::date BETWEEN :start::date AND :end::date
+GROUP BY event
+ORDER BY event

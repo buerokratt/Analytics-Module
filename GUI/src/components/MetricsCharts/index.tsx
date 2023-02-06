08ea19cfc08ad85cccf5f33ee313e69143580b74
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-02-06 02:56:40 +0200

    - Added Dropdown, Metric Charts

---
+++ GUI/src/components/MetricsCharts/index.tsx
@@ -0,0 +1,56 @@
+import { t } from 'i18next'
+import React, { useState } from 'react'
+import { MdOutlineDownload } from 'react-icons/md'
+import { Button, Card, Icon, Track } from '../../components'
+import DropDown from '../Dropdown'
+import OverviewLineChart from '../OverviewLineChart'
+import './MetricsCharts.scss'
+
+type Props = {
+  title: any  
+  data: any
+}
+
+const MetricsCharts = ({ title, data }: Props) => {
+
+  const [showDropDown, setShowDropDown] = useState<boolean>(false);
+  const charts = ['Tulpdiagramm', 'Sektordiagramm', 'joondiagramm'];
+  const [selectedChart, setSelectedChart] = useState<string>('');
+
+  const translateChartKeys = (obj: any) =>
+  Object.keys(obj).reduce(
+    (acc, key) =>
+        key === 'created'
+          ? acc
+          : {
+              ...acc,
+              ...{ [t(`overview.chart.${key}`)]: obj[key] },
+            },
+      {},
+  )
+
+  return (
+    <Card
+        header={
+          <Track>
+            <h3 style={{ flex: 2 }}>{t(title)}</h3>
+            <Button appearance="text" onClick={() => {}}>
+             <Icon icon={<MdOutlineDownload />} size='small' />
+               {t('feedback.csv')}
+            </Button>
+           <DropDown options={charts} defaultValue='Tulpdiagramm' onChangeSelection={(value) => {setSelectedChart(value)}}></DropDown>
+          </Track>
+        } >
+        {data.length > 0 && (
+          <OverviewLineChart
+            data={data.map((entry: any) => ({
+              ...translateChartKeys(entry),
+              created: new Date(entry.created).toLocaleTimeString('default'),
+            }))}
+          />
+        )}
+      </Card>
+  )
+}
+
+export default MetricsCharts
--- GUI/src/pages/FeedbackPage.tsx
+++ GUI/src/pages/FeedbackPage.tsx
@@ -1,34 +1,45 @@
-import React from 'react';
+import { useTranslation } from 'react-i18next'
+import React, { useState } from 'react';
 import OptionsPanel, { Option } from '../components/MetricAndPeriodOptions';
+import MetricsCharts from '../components/MetricsCharts';
 
-const feedbackMetrics: Option[] = [
-    {
-        id: 'statuses',
-        labelKey: 'feedback.statuses',
-        subOptions: [
-            { id: 'client_left_with_answer', labelKey: 'feedback.status.client_left_with_answer', color: '#f00' },
-            { id: 'client_left_without_answer', labelKey: 'feedback.status.client_left_without_answer', color: '#0f0' },
-            { id: 'terminated', labelKey: 'feedback.status.terminated', color: '#0f0' },
-            { id: 'accepted', labelKey: 'feedback.status.accepted', color: '#0f0' },
-            { id: 'hate_speech', labelKey: 'feedback.status.hate_speech', color: '#0f0' },
-            { id: 'answered_in_other_channel', labelKey: 'feedback.status.answered_in_other_channel', color: '#0f0' },
-            { id: 'other_reasons', labelKey: 'feedback.status.other_reasons', color: '#0f0' },
-        ]
-    },
-    { id: 'burokratt_chats', labelKey: 'feedback.burokratt_chats' },
-    { id: 'advisor_chats', labelKey: 'feedback.advisor_chats' },
-    { id: 'selected_advisor_chats', labelKey: 'feedback.selected_advisor_chats' },
-    { id: 'negative_feedback', labelKey: 'feedback.negative_feedback' },
-];
 
 const FeedbackPage: React.FC = () => {
+    const [chartData, setChartData] = useState([])
+    const [currentMetric, setCurrentMetric] = useState('feedback.statuses')
+    const { t } = useTranslation()
+
+    const feedbackMetrics: Option[] = [
+        {
+            id: 'statuses',
+            labelKey: 'feedback.statuses',
+            subOptions: [
+                { id: 'client_left_with_answer', labelKey: 'feedback.status.client_left_with_answer', color: '#f00' },
+                { id: 'client_left_without_answer', labelKey: 'feedback.status.client_left_without_answer', color: '#0f0' },
+                { id: 'terminated', labelKey: 'feedback.status.terminated', color: '#0f0' },
+                { id: 'accepted', labelKey: 'feedback.status.accepted', color: '#0f0' },
+                { id: 'hate_speech', labelKey: 'feedback.status.hate_speech', color: '#0f0' },
+                { id: 'answered_in_other_channel', labelKey: 'feedback.status.answered_in_other_channel', color: '#0f0' },
+                { id: 'other_reasons', labelKey: 'feedback.status.other_reasons', color: '#0f0' },
+            ]
+        },
+        { id: 'burokratt_chats', labelKey: 'feedback.burokratt_chats' },
+        { id: 'advisor_chats', labelKey: 'feedback.advisor_chats' },
+        { id: 'selected_advisor_chats', labelKey: 'feedback.selected_advisor_chats' },
+        { id: 'negative_feedback', labelKey: 'feedback.negative_feedback' },
+    ];
+
     return (
         <>
-            <h1>Feedback</h1>
+            <h1>{t('menu.feedback')}</h1>
             <OptionsPanel
                 metricOptions={feedbackMetrics}
-                onChange={(config) => console.log(config)}
+                onChange={(config) => {
+                    console.log(config);
+                    setCurrentMetric(`feedback.${config.metric}`);
+                }}
             />
+            <MetricsCharts title={currentMetric} data={chartData}/>
         </>
     )
 }

a2af96cb39283692041941cb08e211312a51b06b
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-02-09 06:50:58 +0200

    - Modified Status Endpoint

--- DSL/Resql/status-all-chats.sql
+++ DSL/Resql/status-all-chats.sql
@@ -1,8 +1,8 @@
-SELECT message.event AS event,
+SELECT date_trunc(:period, chat.created) AS date_time, message.event AS event,
        COUNT(DISTINCT chat.base_id)
 FROM chat JOIN message ON chat.base_id = message.chat_base_id
 AND status = 'ENDED'
 AND message.event IN (:events)
 AND chat.created::date BETWEEN :start::date AND :end::date
-GROUP BY event
+GROUP BY date_time, event
 ORDER BY event

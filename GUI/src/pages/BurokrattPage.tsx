128e5ae1ac93942aef46faca7ae2b716183eec53
baha-a <bhlshrf@gmail.com>
2023-02-16 00:21:16 +0300

    Update translation keys

--- GUI/src/pages/BurokrattPage.tsx
+++ GUI/src/pages/BurokrattPage.tsx
@@ -1,38 +1,37 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { Option } from '../components/MetricAndPeriodOptions'
 
-export const metricOptions = [
-    { id: '1', titleKey: 'burokratt.metric.intents' },
-    { id: '2', titleKey: 'burokratt.metric.sessions' },
-    { id: '3', titleKey: 'burokratt.metric.averageResponseSpeed' },
-    { id: '4', titleKey: 'burokratt.metric.understoodQuestions' },
+const metricOptions: Option[] = [
+    {
+        id: 'intents',
+        labelKey: 'burokratt.intents',
+        subOptions: [
+            { id: 'orderByUsability', labelKey: 'burokratt.orderByUsability', color: 'red' },
+            { id: 'newIntents', labelKey: 'burokratt.newIntents', color: 'blue' },
+            { id: 'modifiedIntents', labelKey: 'burokratt.modifiedIntents', color: 'green' },
+            { id: 'avgChatIntents', labelKey: 'burokratt.avgChatIntents', color: 'pinl' },
+        ]
+    },
+    {
+        id: 'sessions',
+        labelKey: 'burokratt.sessions',
+        subOptions: [
+            { id: 'csa', labelKey: 'burokratt.csa', color: 'red' },
+            { id: 'idle', labelKey: 'burokratt.idle', color: 'blue' },
+            { id: 'closed', labelKey: 'burokratt.closed', color: 'green' },
+        ]
+    },
+    { id: 'averageResponseSpeed', labelKey: 'burokratt.averageResponseSpeed' },
+    { id: 'understoodQuestions', labelKey: 'burokratt.understoodQuestions' },
 ]
-
-export const intentsOptions = [
-    { id: '1', titleKey: 'burokratt.intents.orderByUsability' },
-    { id: '2', titleKey: 'burokratt.intents.newIntents' },
-    { id: '3', titleKey: 'burokratt.intents.modifiedIntents' },
-    { id: '4', titleKey: 'burokratt.intents.avgChatIntents' },
-]
-
-export const sessionsOptions = [
-    { id: '1', titleKey: 'burokratt.sessions.csa' },
-    { id: '2', titleKey: 'burokratt.sessions.idle' },
-    { id: '3', titleKey: 'burokratt.sessions.closed' },
-]
-
-export const periodOptions = [
-    { id: '1', titleKey: 'burokratt.period.today' },
-    { id: '2', titleKey: 'burokratt.period.yesterday' },
-    { id: '3', titleKey: 'burokratt.period.30Days' },
-    { id: '4', titleKey: 'burokratt.period.months' },
-    { id: '5', titleKey: 'burokratt.period.period' },
-]
-
 
 const BurokrattPage: React.FC = () => {
+    const { t } = useTranslation()
+
     return (
         <>
-            <h1>Burokratt</h1>
+            <h1>{t('menu.burokratt')}</h1>
         </>
     )
 }

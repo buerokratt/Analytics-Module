07389f05d175e1e5374745190730aa4ae4660982
= <gett_lostt@bk.ru>
2023-08-23 11:37:14 +0300

    Updated base app routing, upgraded header/menu package to v0.0.3

--- GUI/src/App.tsx
+++ GUI/src/App.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
         loading={null}
         persistor={persistor}
       >
-        <BrowserRouter basename={'/'}>
+        <BrowserRouter basename={'/analytics'}>
           <ToastProvider>
             <RootComponent />
           </ToastProvider>

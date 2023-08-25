348788a177695fb53f3141ab78be4fd172f7c7bf
= <gett_lostt@bk.ru>
2023-08-25 09:38:05 +0300

    Removed all the mocking code, added specifi JWTTOKEN endpoint usage and definition, removed ruuterv1 definition, removed serviceWorker usage.

--- GUI/src/components/services/api.ts
+++ GUI/src/components/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.BASE_URL + import.meta.env.REACT_APP_BASE_API_PATH,
+  baseURL: import.meta.env.BASE_URL,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',

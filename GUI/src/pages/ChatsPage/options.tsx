634771bcbbcafce1df162c8f9fb55d2581ffaaea
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-09-07 13:54:04 +0300

    Added Total Chats Count

--- GUI/src/pages/ChatsPage/options.tsx
+++ GUI/src/pages/ChatsPage/options.tsx
@@ -8,6 +8,7 @@ export const chatOptions: Option[] = [
     subOptions: [
       { id: 'byk', labelKey: 'chats.onlyBYK', color: '#fdbf47' },
       { id: 'csa', labelKey: 'chats.csaInvolved', color: '#ed7d32' },
+      { id: 'totalCount', labelKey: 'chats.totalCount', color: '#008000' },
     ],
     unit: t('units.chats') ?? 'chats',
   },
@@ -18,6 +19,7 @@ export const chatOptions: Option[] = [
       { id: 'outside-working-hours', labelKey: 'chats.outsideWorkingHours', color: '#fdbf47' },
       { id: 'long-waiting-time', labelKey: 'chats.longWaitingTime', color: '#ed7d32' },
       { id: 'all-csas-away', labelKey: 'chats.allCsvAway', color: '#8ab4d5' },
+      { id: 'totalCount', labelKey: 'chats.totalCount', color: '#008000' },
     ],
     unit: t('units.chats') ?? 'chats',
   },

2750b219f1fe0deb424759792ecd6b8c19d95b1d
Ahmed yasser <ahmedyasser.dev@hotmail.com>
2023-09-07 10:20:27 +0300

    Bug Metrics and graphics under menu button Vestlused (chats) (#105)
    * Fixed created casting to date
    
    * Enhanced Charts Height
    
    * Fixed Charts Width
    
    * Enhanced Pie Chart Percentages

--- GUI/src/components/BarGraph/index.tsx
+++ GUI/src/components/BarGraph/index.tsx
@@ -1,46 +1,45 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { BarChart, XAxis, CartesianGrid, YAxis, Tooltip, Legend, Bar, Label } from 'recharts'
-import { chartDataKey, dateFormatter, formatDate, getColor, getTicks, round } from '../../util/charts-utils'
-import { GroupByPeriod } from '../MetricAndPeriodOptions/types'
+import React, { useEffect, useRef, useState } from 'react';
+import { BarChart, XAxis, CartesianGrid, YAxis, Tooltip, Legend, Bar, Label, ResponsiveContainer } from 'recharts';
+import { chartDataKey, dateFormatter, formatDate, getColor, getTicks, round } from '../../util/charts-utils';
+import { GroupByPeriod } from '../MetricAndPeriodOptions/types';
 
 type Props = {
-  data: any
-  startDate: string
-  endDate: string
-  unit?: string
-  groupByPeriod: GroupByPeriod
-}
+  data: any;
+  startDate: string;
+  endDate: string;
+  unit?: string;
+  groupByPeriod: GroupByPeriod;
+};
 
 const BarGraph: React.FC<Props> = ({ startDate, endDate, data, unit, groupByPeriod }) => {
-  const [width, setWidth] = useState<number>(10)
-  const ref = useRef<HTMLDivElement>(null)
+  const [width, setWidth] = useState<number | null>(null);
+  const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const handleResize = () => {
-      setWidth(ref.current?.clientWidth ?? 0)
-    }
-    handleResize()
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
+      setWidth(ref.current?.clientWidth ?? 0);
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-  let minDate = new Date(startDate).getTime()
+  let minDate = new Date(startDate).getTime();
   if (groupByPeriod === 'day') {
     const millisecondInOneDay = 24 * 60 * 60 * 1000;
     minDate = minDate - millisecondInOneDay;
   }
 
-
-  const domain = [minDate, new Date(endDate).getTime()]
-  const ticks = getTicks(startDate, endDate, new Date(startDate), new Date(endDate), 5)
+  const domain = [minDate, new Date(endDate).getTime()];
+  const ticks = getTicks(startDate, endDate, new Date(startDate), new Date(endDate), 5);
 
   return (
     <div ref={ref}>
       <BarChart
-        width={width}
-        height={width / 2.8}
+        width={width ?? 0}
+        height={(width ?? 0) / 3.8}
         data={data.chartData}
-        margin={{ top: 20, right: 65, bottom: 70 }}
+        margin={{ top: 20, right: 65, bottom: 50 }}
       >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis
@@ -69,8 +68,7 @@ const BarGraph: React.FC<Props> = ({ startDate, endDate, data, unit, groupByPeri
           cursor={false}
         />
         <Legend wrapperStyle={{ position: 'relative', marginTop: '20px' }} />
-        {
-          (data?.chartData?.length > 0 ?? false) &&
+        {(data?.chartData?.length > 0 ?? false) &&
           Object.keys(data.chartData[0]).map((k, i) => {
             return k === chartDataKey ? null : (
               <Bar
@@ -81,13 +79,12 @@ const BarGraph: React.FC<Props> = ({ startDate, endDate, data, unit, groupByPeri
                 stroke={getColor(data, k)}
                 fill={getColor(data, k)}
               />
-            )
-          })
-        }
+            );
+          })}
         <Tooltip />
-      </BarChart >
-    </div >
-  )
-}
+      </BarChart>
+    </div>
+  );
+};
 
-export default BarGraph
+export default BarGraph;
--- GUI/src/util/api-response-handler.tsx
+++ GUI/src/util/api-response-handler.tsx
@@ -1,7 +1,7 @@
-import axios from "axios"
-import { t } from "i18next"
-import { SubOption } from "../components/MetricAndPeriodOptions/types"
-import { chartDataKey } from "./charts-utils"
+import axios from 'axios';
+import { t } from 'i18next';
+import { SubOption } from '../components/MetricAndPeriodOptions/types';
+import { chartDataKey } from './charts-utils';
 
 export const fetchChartDataWithSubOptions = async (
   url: string,
@@ -10,73 +10,69 @@ export const fetchChartDataWithSubOptions = async (
   isPercentage?: boolean
 ) => {
   try {
-    const result = await axios.post(url,
-      {
-        start_date: config?.start,
-        end_date: config?.end,
-        period: config?.groupByPeriod ?? 'day',
-        options: config?.options.join(',') ?? ''
-      })
+    const result = await axios.post(url, {
+      start_date: config?.start,
+      end_date: config?.end,
+      period: config?.groupByPeriod ?? 'day',
+      options: config?.options.join(',') ?? '',
+    });
 
-    const chartData = result.data.response
-      .reduce((acc: any, row: any, index: number) => {
-        row.forEach((obj: any) => {
-          const { time, ...value } = obj
-          const newObj = {
-            [chartDataKey]: new Date(time).getTime(),
-            [t(subOptions[index].labelKey)]: value[Object.keys(value)[0]]
-          }
-          const existingObj = acc.find((item: any) => item[chartDataKey] === newObj[chartDataKey]);
-          if (existingObj)
-            Object.assign(existingObj, newObj)
-          else
-            acc.push(newObj)
-        })
-        return acc
-      }, [])
+    const chartData = result.data.response.reduce((acc: any, row: any, index: number) => {
+      row.forEach((obj: any) => {
+        const { time, ...value } = obj;
+        const newObj = {
+          [chartDataKey]: new Date(time).getTime(),
+          [t(subOptions[index].labelKey)]: value[Object.keys(value)[0]],
+        };
+        const existingObj = acc.find((item: any) => item[chartDataKey] === newObj[chartDataKey]);
+        if (existingObj) Object.assign(existingObj, newObj);
+        else acc.push(newObj);
+      });
+      return acc;
+    }, []);
 
-    const colors = subOptions.map(x => ({ id: t(x.labelKey), color: x.color }))
+    const colors = subOptions.map((x) => ({ id: t(x.labelKey), color: x.color }));
 
     if (isPercentage === true) {
       const percentagesResponse = chartData.reduce(function (a: any, b: any) {
         const res: any = {};
         Object.keys(chartData[0]).forEach((e: string) => {
           if (e != chartDataKey) {
-            res[e] = a[e] + b[e];
+            res[e] = a[e] ?? 0 + b[e] ?? 0;
           }
-        })
+        });
         return res;
-      })
+      });
 
       const percentagesData: any[] = [];
       for (const key in percentagesResponse) {
-        const currentPercentage: any = {}
+        const currentPercentage: any = {};
         currentPercentage['name'] = key;
-        currentPercentage['value'] = parseFloat(((percentagesResponse[key] / Object.values(percentagesResponse).reduce<number>((a: any, b: any) => a + b, 0)) * 100).toFixed(1));
+        currentPercentage['value'] = parseFloat(
+          (
+            (percentagesResponse[key] /
+              Object.values(percentagesResponse).reduce<number>((a: any, b: any) => a + b, 0)) *
+            100
+          ).toFixed(1)
+        );
         percentagesData.push(currentPercentage);
       }
-      return { chartData, percentagesData, colors }
+      return { chartData, percentagesData, colors };
     } else {
-      return { chartData, colors }
+      return { chartData, colors };
     }
   } catch (_) {
-    return {}
+    return {};
   }
-}
+};
 
-export const fetchChartData = async (
-  url: string,
-  config: any,
-  resultId: string,
-  resultColor = '#fdbf47'
-) => {
+export const fetchChartData = async (url: string, config: any, resultId: string, resultColor = '#fdbf47') => {
   try {
-    const result = await axios.post(url,
-      {
-        start_date: config?.start,
-        end_date: config?.end,
-        period: config?.groupByPeriod ?? 'day'
-      })
+    const result = await axios.post(url, {
+      start_date: config?.start,
+      end_date: config?.end,
+      period: config?.groupByPeriod ?? 'day',
+    });
 
     const response = result.data.response.map((entry: any) => {
       const { time, ...value } = entry;
@@ -84,14 +80,14 @@ export const fetchChartData = async (
       return {
         [t(resultId)]: value[Object.keys(value)[0]],
         [chartDataKey]: new Date(time).getTime(),
-      }
-    })
+      };
+    });
 
     return {
       chartData: response,
       colors: [{ id: t(resultId), color: resultColor }],
-    }
+    };
   } catch (_) {
-    return {}
+    return {};
   }
-}
+};

da3d211a52f6be4be7f547c47ce7cbd3fd8f989a
MarkoKull <42539481+MarkoKull@users.noreply.github.com>
2023-10-13 13:17:01 +0300

    83 apply header and navigation menu (#123)
    * Updated header component version.
    
    * Fixed SERVICE_ID value
    
    * Removed unused components. Made service_id as array
    
    * Added missing header packed package

--- GUI/src/components/index.tsx
+++ GUI/src/components/index.tsx
@@ -1,7 +1,5 @@
 import Layout from './Layout'
 import Button from './Button'
-import Header from './Header'
-import MainNavigation from './MainNavigation'
 import Icon from './Icon'
 import Track from './Track'
 import {
@@ -26,8 +24,6 @@ import Section from './Section'
 export {
   Layout,
   Button,
-  Header,
-  MainNavigation,
   Icon,
   Track,
   Tooltip,

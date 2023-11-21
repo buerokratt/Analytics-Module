9245dfd4e1aca31ed68c3b4d663916e8b739498a
Vassili Moskaljov <112167412+ExiRain@users.noreply.github.com>
2023-11-21 10:44:53 +0200

    Applied Header v0.28, (#134)
    updated store, added chat type, updated generic mocks, updated package.json and Dockerfile.dev

--- GUI/src/App.tsx
+++ GUI/src/App.tsx
@@ -6,25 +6,25 @@ import { ToastProvider } from './components/Toast/ToastContext'
 import RootComponent from './RootComponent'
 import { persistor, store as reducerStore} from './store/reducers/store'
 import {UserInfo} from "./types/userInfo";
-import auth from "./components/services/auth"
-import useUserInfoStore from "./store/user/store";
+import useStore from "./store/user/store";
 import { useQuery } from "@tanstack/react-query";
 
 const App: React.FC = () => {
-
-    const userInfoStore = useUserInfoStore();
-
     if(import.meta.env.REACT_APP_LOCAL === 'true') {
-
         const { data } = useQuery<UserInfo>({
             queryKey: ['cs-custom-jwt-userinfo', 'prod'],
-            onSuccess: (res: any) => userInfoStore.setUserInfo(res)
+            onSuccess: (res: any) => useStore.getState().setUserInfo(res)
         })
     } else {
         const { data: userInfo } = useQuery<UserInfo>({
             queryKey: [import.meta.env.REACT_APP_AUTH_PATH, 'auth'],
-            onSuccess: (data: { data: { custom_jwt_userinfo: UserInfo } }) =>
-                userInfoStore.setUserInfo(data.data.custom_jwt_userinfo),
+            onSuccess: (data: { data: { custom_jwt_userinfo: UserInfo } }) => {
+                localStorage.setItem(
+                    'exp',
+                    data.data.custom_jwt_userinfo.JWTExpirationTimestamp
+                );
+                return useStore.getState().setUserInfo(data.data.custom_jwt_userinfo);
+            }
         });
     }
 

72b9dc82079aafdc1dc140835de35293136bb65d
ahmedyasser <ahmedyasser.dev@hotmail.com>
2024-01-15 06:33:14 +0200

    Modified Average Chat Time Metric

--- GUI/src/pages/AdvisorsPage.tsx
+++ GUI/src/pages/AdvisorsPage.tsx
@@ -245,8 +245,6 @@ const AdvisorsPage: React.FC = () => {
           };
         });
 
-      console.log(advisorsList);
-
       if (advisorsList.length > advisors.current.length) {
         const updatedMetrics = [...advisorsMetrics];
         updatedMetrics[3].subOptions = advisorsList;
@@ -333,6 +331,7 @@ const AdvisorsPage: React.FC = () => {
   const fetchAverageCsaChatTime = async (config: any) => {
     let chartData = {};
     try {
+      const excluded_csas = advisors.current.map((e) => e.id).filter((e) => !config?.options.includes(e));
       const result: any = await request({
         url: getCsaAvgChatTime(),
         method: Methods.post,
@@ -340,17 +339,99 @@ const AdvisorsPage: React.FC = () => {
           metric: config?.groupByPeriod ?? 'day',
           start_date: config?.start,
           end_date: config?.end,
+          excluded_csas: excluded_csas.length ?? 0 > 0 ? excluded_csas : [''],
         },
       });
 
-      const response = result.response.map((entry: any) => ({
-        ...translateChartKeys(entry, chartDataKey),
-        [chartDataKey]: new Date(entry[chartDataKey]).getTime(),
-      }));
+      const res = result.response;
+
+      const advisorsList = Array.from(new Set(res.map((advisor: any) => advisor.customerSupportId)))
+        .map((id: any) => res.find((e: any) => e.customerSupportId == id))
+        .map((e) => {
+          return {
+            id: e?.customerSupportId ?? '',
+            labelKey: e?.customerSupportDisplayName ?? '',
+            color: randomColor(),
+            isSelected: true,
+          };
+        });
+
+      if (advisorsList.length > advisors.current.length) {
+        const updatedMetrics = [...advisorsMetrics];
+        updatedMetrics[4].subOptions = advisorsList;
+        advisors.current = advisorsList;
+        setAdvisorsMetrics(updatedMetrics);
+        setAdvisorsList(advisors.current);
+      } else if (advisors.current.length === 0) {
+        const updatedMetrics = [...advisorsMetrics];
+        updatedMetrics[4].subOptions = [];
+        advisors.current = [];
+        setAdvisorsMetrics(updatedMetrics);
+        setAdvisorsList([]);
+      }
+
+      const response = res
+        .flat(1)
+        .map((entry: any) => ({
+          ...translateChartKeys(entry, chartDataKey),
+          [chartDataKey]: new Date(entry[chartDataKey]).getTime(),
+        }))
+        .reduce((a: any, b: any) => {
+          const dateRow = a.find((i: any) => i[chartDataKey] === b[chartDataKey]);
+          if (dateRow) {
+            dateRow[b[t('chart.customerSupportDisplayName')]] = b[t('chart.avgMin')];
+          } else {
+            a.push({
+              [chartDataKey]: b[chartDataKey],
+              [b[t('chart.customerSupportDisplayName')]]: b[t('chart.avgMin')],
+            });
+          }
+          return a;
+        }, []);
+
+      const chartResponse = response.map((e: any) => {
+        const res = { ...e };
+        advisorsList.forEach((i) => {
+          if (!(i.labelKey in e)) {
+            res[i.labelKey] = 0;
+          }
+        });
+        return res;
+      });
+
+      const percentagesResponse = chartResponse.reduce(function (a: any, b: any) {
+        const res: any = {};
+        Object.keys(chartResponse[0]).forEach((e: string) => {
+          if (e != 'dateTime') {
+            res[e] = a[e] + b[e];
+          }
+        });
+        return res;
+      });
+
+      const percentages: any[] = [];
+      for (const key in percentagesResponse) {
+        const currentPercentage: any = {};
+        currentPercentage['name'] = key;
+        currentPercentage['value'] = parseFloat(
+          (
+            (percentagesResponse[key] /
+              Object.values(percentagesResponse).reduce<number>((a: any, b: any) => a + b, 0)) *
+            100
+          ).toFixed(1)
+        );
+        percentages.push(currentPercentage);
+      }
 
       chartData = {
-        chartData: response,
-        colors: [{ id: 'Average (Min)', color: '#FFB511' }],
+        chartData: chartResponse,
+        percentagesData: percentages,
+        colors: advisorsMetrics[4].subOptions!.map(({ labelKey, color }) => {
+          return {
+            id: labelKey,
+            color,
+          };
+        }),
       };
     } catch (_) {
       //error
@@ -367,7 +448,11 @@ const AdvisorsPage: React.FC = () => {
         onChange={(config) => {
           setConfigs(config);
           configsSubject.next(config);
+          if (currentMetric != `advisors.${config.metric}`) {
+            advisors.current = [];
+          }
           setCurrentMetric(`advisors.${config.metric}`);
+          setAdvisorsList([]);
 
           const selectedOption = advisorsMetrics.find((x) => x.id === config.metric);
           if (!selectedOption) return;

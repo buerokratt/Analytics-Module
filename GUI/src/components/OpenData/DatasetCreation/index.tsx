36990979443b08eefaa1f3c877347ed11af09f46
ahmedyasser <ahmedyasser.dev@hotmail.com>
2024-01-22 13:35:50 +0200

    Fixed Values

--- GUI/src/components/OpenData/DatasetCreation/index.tsx
+++ GUI/src/components/OpenData/DatasetCreation/index.tsx
@@ -73,7 +73,7 @@ const DatasetCreation = ({ metrics, start, end, onClose, existingDataset }: Data
 
     try {
       if (existingDataset === true) {
-        await request({ url: openDataDataset(), method: Methods.post, data: { ...data, metrics, start, end } });
+        await request({ url: openDataDataset(), method: Methods.get, data: { ...data, metrics, start, end } });
       } else {
         await request({
           url: editScheduledReport(),

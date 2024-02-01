ed1c578663d4ae84490251e85f618c39ec1eb416
ahmedyasser <26207361+1AhmedYasser@users.noreply.github.com>
2024-02-01 16:39:23 +0200

    Removed Job Id

--- DSL/Resql/add-scheduled-report.sql
+++ DSL/Resql/add-scheduled-report.sql
@@ -1,3 +1,3 @@
-INSERT INTO scheduled_reports (job_id, "name", "period", metrics, dataset_id, start_date, end_date)
-VALUES (:job_id, :name, :period, array[ :metrics ], :dataset_id, :start_date, :end_date)
+INSERT INTO scheduled_reports ("name", "period", metrics, dataset_id, start_date, end_date)
+VALUES (:name, :period, array[ :metrics ], :dataset_id, :start_date, :end_date)
 RETURNING *;

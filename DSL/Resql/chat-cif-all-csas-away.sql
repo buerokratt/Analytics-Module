308e1349ab0e5e426886c781b09098b3c2439063
baha-a <bhlshrf@gmail.com>
2024-03-14 17:39:24 +0100

    Check if user provided contact info without message events

--- DSL/Resql/chat-cif-all-csas-away.sql
+++ DSL/Resql/chat-cif-all-csas-away.sql
@@ -3,8 +3,12 @@ SELECT
   COUNT(DISTINCT c.base_id) AS chat_count
 FROM chat c
 JOIN message m ON c.base_id = m.chat_base_id 
-WHERE m.event LIKE '%contact-information-fulfilled'
-  AND c.created::date BETWEEN :start::date AND :end::date
+WHERE c.created::date BETWEEN :start::date AND :end::date
+  AND (
+    m.event LIKE '%contact-information-fulfilled' OR
+    (c.end_user_email <> '' AND c.end_user_email IS NOT NULL) OR
+    (c.end_user_phone <> '' AND c.end_user_phone IS NOT NULL)
+  )
   AND 0 < (
     SELECT COUNT(DISTINCT csa.id_code)
     FROM customer_support_agent_activity csa
--- DSL/Resql/chat-cif-outside-working-hours.sql
+++ DSL/Resql/chat-cif-outside-working-hours.sql
@@ -19,8 +19,12 @@ SELECT
   COUNT(DISTINCT c.base_id) AS chat_count
 FROM chat c
 JOIN message m ON c.base_id = m.chat_base_id 
-WHERE m.event LIKE '%contact-information-fulfilled'
-  AND c.created::date BETWEEN :start::date AND :end::date
+WHERE c.created::date BETWEEN :start::date AND :end::date
+  AND (
+    m.event LIKE '%contact-information-fulfilled' OR
+    (c.end_user_email <> '' AND c.end_user_email IS NOT NULL) OR
+    (c.end_user_phone <> '' AND c.end_user_phone IS NOT NULL)
+  )
   AND (
     EXTRACT(HOUR FROM m.created)
     NOT BETWEEN

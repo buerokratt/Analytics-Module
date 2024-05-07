1f0fd71d4bec5628ae59ae6d09766111e36c2ebe
ahmedyasser <26207361+1AhmedYasser@users.noreply.github.com>
2024-05-07 17:43:35 +0300

    Added CSP

--- GUI/vite.config.ts
+++ GUI/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig,loadEnv } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
@@ -7,30 +7,34 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-    process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
 
-    return defineConfig({
-        envPrefix: 'REACT_APP_',
-        plugins: [react(), tsconfigPaths(), svgr()],
-        base: '/analytics',
-        server: {
-            watch: {
-                usePolling: true,
-            },
-            host: true,
-            strictPort: true,
-            port: parseInt(process.env.REACT_APP_APP_PORT)
-        },
-        build: {
-            outDir: './build',
-            target: 'es2015',
-            emptyOutDir: true,
-        },
-        resolve: {
-            alias: {
-                '~@fontsource': path.resolve(__dirname, 'node_modules/@fontsource'),
-                '@': `${path.resolve(__dirname, './src')}`,
-            },
-        }
-    });
-}
+  return defineConfig({
+    envPrefix: 'REACT_APP_',
+    plugins: [react(), tsconfigPaths(), svgr()],
+    base: '/analytics',
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      strictPort: true,
+      port: parseInt(process.env.REACT_APP_APP_PORT),
+      headers: {
+        'Content-Security-Policy':
+          "upgrade-insecure-requests; default-src 'self'; font-src 'self' data:; img-src 'self' data:; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; object-src 'none'; connect-src 'self' http://localhost:8080 http://localhost:8085 https://admin.dev.buerokratt.ee/chat/menu.json;",
+      },
+    },
+    build: {
+      outDir: './build',
+      target: 'es2015',
+      emptyOutDir: true,
+    },
+    resolve: {
+      alias: {
+        '~@fontsource': path.resolve(__dirname, 'node_modules/@fontsource'),
+        '@': `${path.resolve(__dirname, './src')}`,
+      },
+    },
+  });
+};

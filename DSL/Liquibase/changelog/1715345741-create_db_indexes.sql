c63af5e5d0e1c36302e653008650dc1fc27107ef
baha-a <bhlshrf@gmail.com>
2024-05-13 08:55:43 +0300

    Create db indexes

---
+++ DSL/Liquibase/changelog/1715345741-create_db_indexes.sql
@@ -0,0 +1,4 @@
+-- liquibase formatted sql
+-- changeset baha-a:1715345741
+CREATE INDEX IF NOT EXISTS idx_chat_created ON chat (created);
+CREATE INDEX IF NOT EXISTS idx_message_chat_base_id ON message (chat_base_id);

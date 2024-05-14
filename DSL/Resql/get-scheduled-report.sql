d3475e2af13640bb43dfa19b796bc1ca3800224d
baha-a <bhlshrf@gmail.com>
2024-05-14 11:23:39 +0300

    Replace DELETE with INSERT

--- DSL/Resql/get-scheduled-report.sql
+++ DSL/Resql/get-scheduled-report.sql
@@ -1,3 +1,5 @@
 SELECT *
 FROM scheduled_reports
 WHERE dataset_id = :datasetId
+ORDER BY id DESC
+LIMIT 1;
--- DSL/Resql/get-scheduled-reports.sql
+++ DSL/Resql/get-scheduled-reports.sql
@@ -1,2 +1,9 @@
+WITH MaxReports AS (
+  SELECT MAX(id) maxId
+  FROM scheduled_reports
+  GROUP BY dataset_id
+)
 SELECT *
 FROM scheduled_reports
+JOIN MaxReports on id = maxId
+WHERE NOT deleted;

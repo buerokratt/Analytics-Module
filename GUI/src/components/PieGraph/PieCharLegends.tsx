5fdecfeb31fa5aa936698ffa47529cc0a3df60ec
baha-a <bhlshrf@gmail.com>
2024-05-16 21:21:39 +0300

    Fix tooltips and add PieChartLegends components

---
+++ GUI/src/components/PieGraph/PieCharLegends.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { getColor } from '../../util/charts-utils';
+import Track from '../Track';
+import './PieGraph.scss';
+
+type Props = {
+  data: any;
+  percentages: any;
+};
+
+const PieCharLegends = ({ data, percentages }: Props) => {
+  return (
+    <Track
+      direction="vertical"
+      flex={20}
+      align="left"
+      isFlex
+      isMultiline
+    >
+      {
+        percentages?.map((e: any) => {
+          const color = getColor(data, e.name);
+
+          return (
+            <Track key={`track-${e.name}`}>
+              <div
+                className="legend_circle"
+                style={{ backgroundColor: color }}
+              />
+              <label style={{ color, maxLines: 1 }}>
+                {`${e.name}: ${e.value} %`}
+              </label>
+            </Track>
+          )})
+      }
+    </Track>
+  );
+};
+
+export default PieCharLegends;

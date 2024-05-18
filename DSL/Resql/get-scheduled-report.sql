479ffc6ffdd6fb953073c64130c6b73b2a41520c
Bahaa Alsharif <bhlshrf@gmail.com>
2024-05-18 20:06:41 +0300

    Replace SELECT * with specific fields (#225)

--- DSL/Resql/get-scheduled-report.sql
+++ DSL/Resql/get-scheduled-report.sql
@@ -1,3 +1,12 @@
-SELECT *
+SELECT 
+  id,
+  name,
+  dataset_id,
+  period,
+  metrics,
+  created,
+  updated,
+  start_date,
+  end_date
 FROM scheduled_reports
-WHERE dataset_id = :datasetId
+WHERE dataset_id = :datasetId;

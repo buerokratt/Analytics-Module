9861fea68a0c28462dfa4fb8a449b39f48359f74
ahmedyasser <26207361+1AhmedYasser@users.noreply.github.com>
2024-05-28 13:28:03 +0300

    Modified Lower case desc

--- DSL/Resql/feedback-chats-with-negative-feedback.sql
+++ DSL/Resql/feedback-chats-with-negative-feedback.sql
@@ -27,6 +27,6 @@ LEFT JOIN chat ON n_chats.base_id = chat.base_id
 JOIN c_chat ON c_chat.base_id = chat.base_id AND n_chats.created = chat.created
 WHERE chat.feedback_rating IS NOT NULL
 AND chat.ended IS NOT null
-ORDER BY c_chat.created desc
+ORDER BY c_chat.created DESC
 OFFSET ((GREATEST(:page, 1) - 1) * :page_size) LIMIT :page_size;
 

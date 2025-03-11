c29495b5920c1bd5b5836181c7fcc90d37da3683
exiledrain <vasssilimoskaljov@gmail.com>
2025-03-11 15:55:36 +0200

    Added checking for chatbot conversations

--- DSL/Resql/analytics/POST/chat-count-only-chatbot.sql
+++ DSL/Resql/analytics/POST/chat-count-only-chatbot.sql
@@ -4,10 +4,11 @@ SELECT
 FROM chat
 WHERE ended::date BETWEEN :start::date AND :end::date AND status = 'ENDED'
 AND NOT EXISTS (
-    SELECT 1
+    SELECT *
     FROM message
     WHERE message.chat_base_id = chat.base_id
     AND message.author_role = 'backoffice-user'
+    AND message.content = ''
 )
 GROUP BY time
 ORDER BY time

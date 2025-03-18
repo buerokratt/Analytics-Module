4233acfe318fd19a5465a87953380f633c779ff8
Igor Krupenja <igor.krupenja@gmail.com>
2025-03-18 14:44:15 +0200

    Remove message

--- DSL/Resql/analytics/POST/feedback-csa-chats-feedback-nps.sql
+++ DSL/Resql/analytics/POST/feedback-csa-chats-feedback-nps.sql
@@ -14,12 +14,6 @@ WITH chat_csas AS (
             SELECT 1
             FROM message
             WHERE message.chat_base_id = chat.base_id
-                AND message.author_role = 'backoffice-user'
-        )
-        AND EXISTS (
-            SELECT 1
-            FROM message
-            WHERE message.chat_base_id = chat.base_id
                 AND message.author_role = 'end-user'
         )
         AND STATUS = 'ENDED'

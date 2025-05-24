91818edd00c4f0bcbc9e02c6bf62094be28eddba
ahmer-mt <ahmer.ali@mindtitan.com>
2025-05-24 17:52:35 +0500

    SQL-005: remove message name fields from denormalized metrics table

--- DSL/Resql/analytics/POST/csa-avg-chat-time.sql
+++ DSL/Resql/analytics/POST/csa-avg-chat-time.sql
@@ -3,9 +3,6 @@ WITH distinct_chats AS (
     chat_base_id AS base_id, 
     date_trunc(:metric, created) AS date_time, 
     message_author_id AS author_id,
-    message_author_display_name AS display_name,
-    message_author_user_first_name AS first_name,
-    message_author_user_last_name AS last_name,
     age(last_message_timestamp, first_message_timestamp) AS chat_length
   FROM denormalized_chat_messages_for_metrics
   WHERE 
@@ -21,8 +18,6 @@ WITH distinct_chats AS (
 
 SELECT
     date_time,
-    MAX(display_name) AS customer_support_display_name,
-    MAX(CONCAT(first_name, ' ', last_name)) AS customer_support_full_name,
     MAX(author_id) AS customer_support_id,
     ROUND(EXTRACT(epoch FROM COALESCE(AVG(chat_length), '0 seconds'::interval)) / 60) AS avg_min
 FROM distinct_chats
--- DSL/Resql/analytics/POST/csa-chats-count.sql
+++ DSL/Resql/analytics/POST/csa-chats-count.sql
@@ -3,9 +3,6 @@ WITH FinalData AS (
         date_trunc(:metric, created) AS date_time,
         chat_base_id,
         message_author_id,
-        message_author_display_name,
-        message_author_user_first_name,
-        message_author_user_last_name,
         message_author_id AS id_code
     FROM denormalized_chat_messages_for_metrics
     WHERE message_author_role IN ('backoffice-user', 'end-user')
@@ -18,9 +15,7 @@ WITH FinalData AS (
 )
 SELECT
     date_time,
-    MAX(message_author_display_name) AS customer_support_display_name,
     MAX(id_code) AS customer_support_id,
-    MAX(CONCAT(message_author_user_first_name, ' ', message_author_user_last_name)) AS customer_support_full_name,
     COUNT(DISTINCT chat_base_id) AS count
 FROM FinalData
 GROUP BY date_time, message_author_id;

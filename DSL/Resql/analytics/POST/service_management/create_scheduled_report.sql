93de124c21a569f69498ff3d81125766a8d4278a
Artsiom Beida <artsiom.beida@mindtitan.com>
2025-05-27 17:46:48 +0200

    add naming-002 for namespcae service_management

--- DSL/Resql/analytics/POST/service_management/create_scheduled_report.sql
+++ DSL/Resql/analytics/POST/service_management/create_scheduled_report.sql
@@ -1,3 +1,3 @@
 INSERT INTO scheduled_reports ("name", "period", metrics, dataset_id, start_date, end_date)
 VALUES (:name, :period, array[ :metrics ], :dataset_id, :start_date, :end_date)
-RETURNING *;
+RETURNING id, name, dataset_id, period, metrics, created, updated, start_date, end_date, deleted;

7cae758bfa5de5db730de35dee02af60262848e6
exiled <vassilimoskaljov@gmail.com>
2025-10-13 12:54:36 +0300

    Updated query to properly get forwarded chats

--- DSL/Resql/analytics/POST/overview-total-forwarded-chats.sql
+++ DSL/Resql/analytics/POST/overview-total-forwarded-chats.sql
@@ -15,13 +15,13 @@ WHERE (:showTest = TRUE OR chat.test = FALSE)
 ORDER BY base_id, ended DESC
     ),
     redirected_chats AS (
-SELECT DISTINCT base_id
+SELECT DISTINCT base_id, updated
 FROM chat
 WHERE status = 'REDIRECTED'
 )
 SELECT COUNT(DISTINCT base_id) AS metric_value
-FROM forwarded_chats_by_csa
-WHERE date_trunc('day', forwarded_chats_by_csa.ended) = date_trunc('day', current_date - '1 day'::INTERVAL)
+FROM redirected_chats
+WHERE date_trunc('day', redirected_chats.updated) = date_trunc('day', current_date - '1 day'::INTERVAL)
 UNION ALL
 SELECT COUNT(DISTINCT base_id) FILTER (
         WHERE external_id IS NOT NULL AND external_id <> ''
@@ -32,7 +32,7 @@ WHERE (
         OR chat.test = FALSE
     )
   AND (
-    array_length(ARRAY[:urls]::TEXT[], 1) IS NULL
-        OR chat.end_user_url LIKE ANY(ARRAY[:urls]::TEXT[])
-    )
+    array_length(ARRAY[:urls]::text[], 1) IS NULL
+        OR (array_length(ARRAY[:urls]::text[], 1) = 1 AND (ARRAY[:urls]::text[])[1] = 'none')
+        OR chat.end_user_url LIKE ANY(ARRAY[:urls]::text[]))
   AND date_trunc('day', chat.ended) = date_trunc('day', current_date - '1 day'::INTERVAL);

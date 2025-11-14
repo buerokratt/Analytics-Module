0d67964652a8dc0cb2908d1611a8fb89f9139819
ffrose <119657383+ffrose@users.noreply.github.com>
2025-11-14 15:36:58 +0200

    Update reorder-array.ts

--- GUI/src/util/reorder-array.ts
+++ GUI/src/util/reorder-array.ts
@@ -2,8 +2,10 @@ export const reorderItem = <T>(arr: T[], predicate: (i: T) => boolean, target: n
   const arrCopy = arr.slice()
   const movingItem = arrCopy.splice(arr.findIndex(predicate), 1)
   arrCopy.splice(target, 0, movingItem[0])
-  return arrCopy.map((item, index) => ({
+  let result =  arrCopy.map((item, index) => ({
     ...item,
     ordinality: index + 1,
   }))
+  console.log('SORTING', result);
+  return result;
 }

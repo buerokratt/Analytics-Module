b2502c494a2356126af8f6429e19b6659b3fe87c
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2026-02-10 01:26:39 +0200

    fix(616): Revert user.ts

--- GUI/src/components/services/user.ts
+++ GUI/src/components/services/user.ts
@@ -1,11 +1,11 @@
-import {DomainSelection} from "../../types/widgetModels";
-import { analyticsApi } from "./api";
+import { DomainSelection } from '../../types/widgetModels';
+import { api } from './api';
 
 export async function getWidgetData(userId: string) {
-    const { data } = await analyticsApi.get<DomainSelection[]>('accounts/widget-data', {
-        params: {
-            user_id: userId,
-        },
-    });
-    return data;
+  const { data } = await api.get<DomainSelection[]>('accounts/widget-data', {
+    params: {
+      user_id: userId,
+    },
+  });
+  return data;
 }
